be26a64ecf09cca9f21124e483f18662556bfa5d
Robin Kennedy-Reid <robin.kennedy-reid@stfc.ac.uk>
2022-07-20 19:35:06 +0100

    ➕ Add buildah

--- on_node_start_ubuntu.sh
+++ on_node_start_ubuntu.sh
@@ -38,7 +38,7 @@ function install_go() {
 
 function build_and_install_podman() {
 
-    apt-get -qy install fuse3 btrfs-progs git go-md2man iptables libassuan-dev libbtrfs-dev libc6-dev libdevmapper-dev libglib2.0-dev libgpgme-dev libgpg-error-dev libprotobuf-dev libprotobuf-c-dev libseccomp-dev libselinux1-dev libsystemd-dev pkg-config runc uidmap
+    apt-get -qy install buildah fuse3 btrfs-progs git go-md2man iptables libassuan-dev libbtrfs-dev libc6-dev libdevmapper-dev libglib2.0-dev libgpgme-dev libgpg-error-dev libprotobuf-dev libprotobuf-c-dev libseccomp-dev libselinux1-dev libsystemd-dev pkg-config runc uidmap
 
     pushd "$(mktemp -d)"
 
